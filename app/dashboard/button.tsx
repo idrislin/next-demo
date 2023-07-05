959d56fde201c010ddf281327474ebdfac6e1739
linjx <2376909468@qq.com>
2023-07-05 17:14:24 +0800

    🎉 init project

---
+++ app/dashboard/button.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import {Button} from 'antd';
+
+const DashboardButton: React.FC = () => {
+  return (
+    <div className='w-full h-full'>
+      <Button type='primary'>Button</Button>
+    </div>
+  );
+};
+
+export default DashboardButton;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// 'use client';
+import React from 'react';
+import DashboardButton from './button';
+
+const Home = () => {
+  console.log('server');
+  return <DashboardButton />;
+};
+
+export default Home;
